ffe0a912bb543c7f8ed2bda3bf2c6ec913d111e9
GLopezSiles <glpzsls@udel.edu>
2024-09-05 19:52:02 -0400

    update - error repair

--- Homework Project/Assets/Scripts/HelloWorld.cs
+++ Homework Project/Assets/Scripts/HelloWorld.cs
@@ -8,6 +8,15 @@ public class NewBehaviourScript : MonoBehaviour
     void Start()
     {
         Debug.Log("Hello, world!");
+        Debug.LogWarning("Are you sure you want to do that?");
+        Debug.LogError("You shouldn't have done that.");
+        // Author: Guadalupe Lopez-Siles 
+        /*Will we have to model or render our own objects to use in our games?
+         * My game idea would be a person having to escap from a mall decorated like the 1970s, '80s and '90s
+         * The development question I had was how long does it take to knock out bugs and glitches during game development time?
+         * 
+         * 
+         */
     }
 
     // Update is called once per frame
@@ -16,3 +25,4 @@ public class NewBehaviourScript : MonoBehaviour
         
     }
 }
+        
